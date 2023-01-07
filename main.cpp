37e534b002e5bc320f1c5be8ba2ebae7424e8893
Geronimo Basso <gerobasso01@gmail.com>
2023-01-07 15:59:02 -0300

    final commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() {
     PilaGen<int> *pilaI = new PilaGen<int>;
     PilaGen<int> *pilaJ = new PilaGen<int>;
+    PilaGen<int> *pilaK = new PilaGen<int>;
     bool condicion = true;
-    int opcionMenu , i , j;
-    cout << "Bienvenido a nuestro programa, ingrese un par de número enteros (i,j), dependiendo de estos datos podremos realizaer diferentes insercciones en las pilas" << endl;
+    int opcionMenu , i , j, k;
+    cout << "Bienvenido a nuestro programa, ingrese un tulpa de número enteros (i,j,k), dependiendo de estos datos podremos realizaer diferentes insercciones en las pilas" << endl;
     while ( condicion ){
-        cout << "Opciones Menu: \n1. Añadir par de números  \n2. Terminar programa" << endl;
+        cout << "Opciones Menu: \n1. Añadir tupla de números  \n2. Terminar programa" << endl;
         cin >> opcionMenu;
         switch ( opcionMenu ) {
             case 1:
@@ -20,22 +21,35 @@ int main() {
                 cout << "Introduzca j: " << endl;
                 cin >> j;
                 pilaJ->insertar_cabeza( j );
-                if ( i > 0 ){ //i es positivo
-                    pilaI->insertar_cabeza( j );
-                }else if ( i == 0 ){ //i igual menos q  cero
-                    cout << "------------- PILA i -------------------" << endl;
-                    int pilaIsize = pilaI->size();
-                    for(int k = 0 ; k < pilaIsize; k++ ){
-                        cout << pilaI->pop() << endl;
+                cout << "Introduzca k: " << endl;
+                cin >> k;
+                pilaK->insertar_cabeza( k );
+                if( k == 0){
+                    if ( i > 0 ){ //i es positivo
+                        pilaI->insertar_cabeza( j );
+                    }else if ( i == 0 ){ //i igual menos q  cero
+                        cout << "------------- PILA i -------------------" << endl;
+                        int pilaIsize = pilaI->size();
+                        for(int k = 0 ; k < pilaIsize; k++ ){
+                            cout << pilaI->pop() << endl;
+                        }
+                        cout << "------------- PILA j -------------------" << endl;
+                        int pilaJsize = pilaJ->size();
+                        for(int m = 0 ; m < pilaJsize; m++ ){
+                            cout << pilaJ->pop() << endl;
+                        }
+                        break;
+                    }else if( i < 0 ){ //i es 0
+                        pilaJ->eliminar_cabeza();
                     }
-                    cout << "------------- PILA j -------------------" << endl;
-                    int pilaJsize = pilaJ->size();
-                    for(int m = 0 ; m < pilaJsize; m++ ){
-                        cout << pilaJ->pop() << endl;
+                }else if ( k == -1){ //borrar la pila K
+                    cout << "------------- PILA K -------------------" << endl;
+                    int pilaKsize = pilaK->size();
+                    for(int m = 0 ; m < pilaKsize; m++ ){
+                        cout << pilaK->pop() << endl;
                     }
-                    break;
-                }else if( i < 0 ){ //i es 0
-                    pilaJ->eliminar_cabeza();
+                }else{
+                    cout << "El valor de K debe ser 0 o -1 " << endl;
                 }
                 break;
             }
